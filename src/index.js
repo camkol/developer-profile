4ed4c5c8224011b92f05352d437d2c4138863467
camkol <121874225+camkol@users.noreply.github.com>
2024-04-11 16:41:36 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skills = [
+  {
+    skill: "HTML",
+    level: "beginner",
+    color: "lightblue",
+  },
+  {
+    skill: "CSS",
+    level: "beginner",
+    color: "red",
+  },
+  {
+    skill: "JavaScript",
+    level: "beginner",
+    color: "orange",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "yellow",
+  },
+  {
+    skill: "SASS",
+    level: "intermediate",
+    color: "pink",
+  },
+  {
+    skill: "Bootstrap",
+    level: "intermediate",
+    color: "purple",
+  },
+  {
+    skill: "Photoshop",
+    level: "intermediate",
+    color: "violet",
+  },
+  {
+    skill: "Node",
+    level: "advanced",
+    color: "Green",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="image">
+      <img src="images/profilephotomini.jpg" alt="" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div className="intro">
+      <h1>Cameron Howze</h1>
+      <p>
+        Full-stack web developer and US veteran. When I am not coding, I love
+        spending time with my amazing son
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "💪"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "😤"}
+      </span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
